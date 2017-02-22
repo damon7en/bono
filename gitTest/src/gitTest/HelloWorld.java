927a844c790016d3f1e1e426f4ea317d23c70be0
it <it@DESKTOP-KKAQ0RK>
2017-02-22 14:50:54 +0900

    message

--- gitTest/src/gitTest/HelloWorld.java
+++ gitTest/src/gitTest/HelloWorld.java
@@ -4,16 +4,11 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-		System.out.println("master");
-		System.out.println("branch");
-		System.out.println("branc1");
-
-		System.out.println("master master");
-
-		System.out.println("branch branch");
-		System.out.println("commit and push");
+		int a = 1;
+		int b = 2;
 		
+				System.out.println("гу :"+(a+b));
+
 	}
 
 }
